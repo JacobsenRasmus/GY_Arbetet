23a2b582e96a9a01901ddfabaa92a9451fed7726
Hanibalbarca12 <42931567+Hanibalbarca12@users.noreply.github.com>
2019-02-28 22:23:06 +0100

    added SuperTiled + broken map

---
+++ ProjektEnhörning/Assets/tiles_dungeon_v1.1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="tiles_dungeon_v1.1" tilewidth="16" tileheight="16" tilecount="480" columns="20">
+ <image source="tiles_dungeon_v1.1.png" width="320" height="384"/>
+</tileset>
