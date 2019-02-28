fefeb709f99350cdd270362d5c6ad684bdb900b7
Rasmus Jacobsen <CarlFilipRasmusJacobsen@gmail.com>
2019-02-28 22:06:02 +0100

    dsfg

---
+++ ProjektEnhörning/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour {
+
+    public float moveSpeed;
+    private bool facingLeft;
+
+	// Use this for initialization
+	void Start () {
+        facingLeft = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Move player right left
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f,0f));
+        // Move player upp down
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f && !facingLeft || Input.GetAxisRaw("Horizontal") < -0.5f && facingLeft)
+        {
+            facingLeft = !facingLeft;
+        }
+    }
+}
