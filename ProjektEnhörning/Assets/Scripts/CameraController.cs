bc1fc52422ecc0afc5294817723e3a00469209e7
Rasmus Jacobsen <CarlFilipRasmusJacobsen@gmail.com>
2019-03-03 22:02:10 +0100

    Camera Follow Player

---
+++ ProjektEnhörning/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+     
+    public GameObject followTarget; //tacke objekt to follow
+    private Vector3 targetPos;      //tack objects position
+    public float moveSpeed;         //camra move speed OBS never now faster then the object 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //finds target position
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        //moves to target pos
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+	}
+}
