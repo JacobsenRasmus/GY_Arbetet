1869b3bba67b9731dc9b7b14f6de6c4d1299b426
Rasmus Jacobsen <CarlFilipRasmusJacobsen@gmail.com>
2019-03-03 18:59:48 +0100

    Player Moving Fixing bugs

--- ProjektEnhörning/Assets/Scripts/PlayerController.cs
+++ ProjektEnhörning/Assets/Scripts/PlayerController.cs
@@ -2,29 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController: MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    public float moveSpeed;
-    private bool facingLeft;
+    public float moveSpeed;     // spelarens hastighet
+    private Animator anim;      // Ger oss till gång till animationerna
+    private bool PlayerMoving;  // Rörsig spelaren Ja nej
+    private Vector2 LastMove;   // tar värdet på senaste riktning vi rörde oss
+    //private bool facingLeft;
 
-	// Use this for initialization
-	void Start () {
-        facingLeft = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        //    facingLeft = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoving = false;
 
         // Move player right left
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-            transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f,0f));
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            PlayerMoving = true;
+            LastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
         // Move player upp down
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            PlayerMoving = true;
+            LastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
 
+        // ger animationer värde
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", PlayerMoving);
+        anim.SetFloat("LastMoveX", LastMove.x);
+        anim.SetFloat("LastMoveY", LastMove.y);
+
+        /*/ ska vrida på spelaren
         if (Input.GetAxisRaw("Horizontal") > 0.5f && !facingLeft || Input.GetAxisRaw("Horizontal") < -0.5f && facingLeft)
         {
             facingLeft = !facingLeft;
-        }
+        }*/
     }
 }
--- ProjektEnhörning/Assets/SuperTiled2Unity-master/docs/auto-run-local.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pushd %~dp0
-
-echo Automatically serving sphinx docs
-sphinx-autobuild . .\_build
-
-IF %ERRORLEVEL% NEQ 0 Echo sphinx-autobuild is not installed. Run 'pip install sphinx-autobuild'
-
-popd
