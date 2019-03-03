1c004ea46769ac2bc1b8921e65797fd3497e83e3
Rasmus Jacobsen <CarlFilipRasmusJacobsen@gmail.com>
2019-03-03 14:50:44 +0100

    Revert "added pariarly functioning map, i think"
    This reverts commit 4526f8f5045f69dffb2f53debd711752e9eded61.

--- ProjektEnhörning/Assets/tiles_dungeon_v1.1.tsx
+++ ProjektEnhörning/Assets/tiles_dungeon_v1.1.tsx
@@ -1,9 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.2" name="tiles_dungeon_v1.1" tilewidth="16" tileheight="16" tilecount="480" columns="20">
  <image source="tiles_dungeon_v1.1.png" width="320" height="384"/>
- <tile id="477">
-  <objectgroup draworder="index">
-   <object id="1" x="-0.5" y="-0.5" width="17.25" height="17"/>
-  </objectgroup>
- </tile>
 </tileset>
