52b9b4ff891e3a2e2abcc1688958a9867ae14bb1
Hanibalbarca12 <42931567+Hanibalbarca12@users.noreply.github.com>
2019-03-04 22:34:16 +0100

    better collsion

--- ProjektEnhörning/Assets/Scripts/PlayerController.cs
+++ ProjektEnhörning/Assets/Scripts/PlayerController.cs
@@ -9,13 +9,18 @@ public class PlayerController : MonoBehaviour
     private Animator anim;      // Ger oss till gång till animationerna
     private bool PlayerMoving;  // Rörsig spelaren Ja nej
     private Vector2 LastMove;   // tar värdet på senaste riktning vi rörde oss
+
+    private Rigidbody2D playerRigidbody;
     //private bool facingLeft;
+
+
 
     // Use this for initialization
     void Start()
     {
         anim = GetComponent<Animator>();
         //    facingLeft = false;
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -26,7 +31,8 @@ public class PlayerController : MonoBehaviour
         // Move player right left
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, playerRigidbody.velocity.y);
             PlayerMoving = true;
             LastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
@@ -34,9 +40,22 @@ public class PlayerController : MonoBehaviour
         // Move player upp down
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
             PlayerMoving = true;
             LastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        //make player stop when releasing button
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            playerRigidbody.velocity = new Vector2(0f, playerRigidbody.velocity.y);
+        }
+
+        //make player stop when releasing button
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
         }
 
         // ger animationer värde
