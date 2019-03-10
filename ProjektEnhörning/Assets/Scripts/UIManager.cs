d59442845d80b3fbb197e27f5bc1e3b62750cd25
Hanibalbarca12 <42931567+Hanibalbarca12@users.noreply.github.com>
2019-03-10 19:06:25 +0100

    UI added

---
+++ ProjektEnhörning/Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Slider healthbar;
+    public Text HPText;
+    public PlayerHealthManager playerHealth;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthbar.maxValue = playerHealth.PlayerMaxHealth;
+        healthbar.value = playerHealth.PlayerCurrentHealth;
+        HPText.text = "HP: " + playerHealth.PlayerCurrentHealth; 
+    }
+}
