3593aed9835c1a3bd9113335e471ae894a842357
Rasmus Jacobsen <CarlFilipRasmusJacobsen@gmail.com>
2019-03-10 18:30:40 +0100

    Revert "fiende animation halv fixad"
    This reverts commit 736d4fb8402e0f3ac043d34f90a672c6e2a2b113.

--- ProjektEnhörning/Assets/Scripts/PlayerController.cs
+++ ProjektEnhörning/Assets/Scripts/PlayerController.cs
@@ -22,8 +22,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
-        playerRigidbody = GetComponent<Rigidbody2D>();
         //    facingLeft = false;
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -33,46 +33,44 @@ public class PlayerController : MonoBehaviour
 
         if(!attacking)
         {
-            // Move player right left
-            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-            {
-                //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-                playerRigidbody.velocity = new Vector2(
-                    Input.GetAxisRaw("Horizontal") * moveSpeed, playerRigidbody.velocity.y);
 
-                PlayerMoving = true;
-                LastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
-            }
+        // Move player right left
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, playerRigidbody.velocity.y);
+            PlayerMoving = true;
+            LastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
 
-            // Move player upp down
-            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-            {
-                //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 
-                    Input.GetAxisRaw("Vertical") * moveSpeed);
-                PlayerMoving = true;
-                LastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
-            }
+        // Move player upp down
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            PlayerMoving = true;
+            LastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
 
-            //make player stop when releasing button
-            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
-            {
-                playerRigidbody.velocity = new Vector2(0f, playerRigidbody.velocity.y);
-            }
+        //make player stop when releasing button
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            playerRigidbody.velocity = new Vector2(0f, playerRigidbody.velocity.y);
+        }
 
-            //make player stop when releasing button
-            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
-            {
-                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
-            }
-            //atttacking
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                attackTimeCounter = attackTime;
-                attacking = true;
-                //playerRigidbody.velocity = Vector2.zero;
-                anim.SetBool("PlayerAttacking", true);
-            }
+        //make player stop when releasing button
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
+        }
+        //atttacking
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            attackTimeCounter = attackTime;
+            attacking = true;
+            //playerRigidbody.velocity = Vector2.zero;
+            anim.SetBool("PlayerAttacking", true);
+        }
         }
 
         if(attackTimeCounter > 0)
